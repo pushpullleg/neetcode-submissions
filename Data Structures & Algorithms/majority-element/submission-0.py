fd379e39ba4efabc2f339bbfce3cf1245be8e1aa
Mukesh Ravichandran <mravichandr@leomail.tamuc.edu>
2026-07-21 17:04:11 -0500

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        my_dict ={}
+
+        for num in nums:
+            my_dict[num] = 1 + my_dict.get(num, 0)
+        
+        return max(my_dict, key=my_dict.get)
